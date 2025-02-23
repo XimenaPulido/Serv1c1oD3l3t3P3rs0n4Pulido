b64912f67aaa897dc77c938797199881197067ab
XimenaPulidoGutierrez <ximena.pulido.gutierrez@gmail.com>
2025-02-22 22:37:57 -0600

    Tarea realizada

---
+++ App.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const personasRoutes = require('./routes/persona.routes');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+app.use('/api/personas', personasRoutes);
+
+//conexión a la base de datos
+
+mongoose.connect('mongodb+srv://20233tn125:Mafalda5532@cluster0.bxn1e.mongodb.net/inventario-db?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Conexión exitosa a la base de datos a MongoDB');
+        app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
+    })
+    .catch((err) => console.log('Error al conectar en MongoDB', err));
